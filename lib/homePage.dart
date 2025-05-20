060e2adea5517e88dceadd09473f5be23ba85ee2
InfernalSpark <shriramkiran05@gmail.com>
2025-05-20 14:02:38 +0530

    completed home page (no logic implemented)

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget{
   const HomePage({super.key});
@@ -9,35 +10,16 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage> {
-
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.all(12.0),
-            child: SearchAnchor(
-              builder: (BuildContext context, SearchController controller) {
-                return SearchBar(
-                  controller: controller,
-                  hintText: "Search Books, eBooks...",
-                  padding: const WidgetStatePropertyAll<EdgeInsets>(
-                    EdgeInsets.symmetric(horizontal: 16.0),
-                  ),
-                  onTap: () {
-                    showSearch(context: context, delegate: BookSearchDelegate());
-                  },
-                  trailing: <Widget>[
-                    Icon(Icons.search)
-                  ],
-                );
-
-              },
-              suggestionsBuilder: (context, controller) => [], // Doesnt look like proper usage
-            ),
+            child: MainSearchBar(),
 
           ),
           Padding(
@@ -83,7 +65,7 @@ class _HomePageState extends State<HomePage> {
                           'assets/book_sharing.jpg',
                           height: 80,
                           width: 80,
-                          fit: BoxFit.cover,
+                          fit: BoxFit.fitHeight,
                         )
                     )
                   ],
@@ -91,14 +73,69 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          Expanded(child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: StatWidget(),
+          Padding(
+            padding: EdgeInsets.all(12),
+            child: StatWidget(),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text("New Arrivals", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500), textAlign: TextAlign.start,),
+          ),
+          Padding(
+            padding: EdgeInsets.all(12),
+            child: SizedBox(
+              height: 250,
+              child: HorizontalBookScroll()
             )
-          )
-
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text("Your Items", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500), textAlign: TextAlign.start,),
+          ),
+          Padding(
+              padding: EdgeInsets.all(12),
+              child: SizedBox(
+                  height: 250,
+                  child: HorizontalBookScroll()
+              )
+          ),
         ],
       ),
+    );
+  }
+}
+
+class MainSearchBar extends StatelessWidget {
+  const MainSearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchAnchor(
+      builder: (BuildContext context, SearchController controller) {
+        return SearchBar(
+          controller: controller,
+          leading: const Icon(Icons.search),
+          hintText: "Search Books, eBooks...",
+          padding: const WidgetStatePropertyAll<EdgeInsets>(
+            EdgeInsets.symmetric(horizontal: 16.0),
+          ),
+          onTap: () {
+            showSearch(context: context, delegate: BookSearchDelegate());
+          },
+          trailing: <Widget>[
+            IconButton(
+              onPressed: () {
+                // TODO : implement notification
+              },
+              icon: Icon(Icons.notifications_none_outlined),
+            )
+          ],
+        );
+    
+      },
+      suggestionsBuilder: (context, controller) => [], // Doesnt look like proper usage
     );
   }
 }
@@ -114,11 +151,12 @@ class StatWidget extends StatefulWidget {
 class _StatWidgetState extends State<StatWidget>{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return SizedBox(
+      height: 262,
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Your stats:", style: TextStyle(fontSize: 18),),
+          Text("Your stats:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
           SizedBox(height: 16,),
           Container(
             padding: EdgeInsets.all(16),
@@ -240,4 +278,53 @@ class BookSearchDelegate extends SearchDelegate<String> {
       },
     );
   }
+}
+
+
+class HorizontalBookScroll extends StatelessWidget{
+  const HorizontalBookScroll({super.key});// TODO : add argument to take data from backend
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder( // TODO : rewrite function to use backend data
+        padding: EdgeInsets.zero,
+        scrollDirection: Axis.horizontal,
+        itemCount: 7,
+        itemBuilder: (BuildContext context, int index) {
+          return SizedBox(
+            height: 120, // idk what changes this height value does
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)
+              ),
+              elevation: 0,
+              color: Theme.of(context).canvasColor,
+              child: Container(
+                width: 145,
+                padding: EdgeInsets.zero,
+
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset("assets/stats_book_temp.png", width: 145, height: 201, fit: BoxFit.fill,),
+
+                    ),
+                    SizedBox(height: 8,),
+                    Text("Book $index", textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+    );
+  }
+
 }
