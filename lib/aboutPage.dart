cbd529264081b8c05d66da13c1dd09fcd0653e0b
sarangt123 <sarangtprvt@gmail.com>
2026-06-25 23:37:54 +0530

    Fix 12 : Inline contact info on About page, remove separate ContactPage and Follow Us section

--- lib/aboutPage.dart
+++ lib/aboutPage.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:library_nitc/models/book_arrangement.dart';
 import 'package:library_nitc/models/business_hours.dart';
 import 'package:library_nitc/services/opac_home_service.dart';
-import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 int _dayIndex(String abbr) {
   const map = {'sun': 7, 'mon': 1, 'tue': 2, 'wed': 3, 'thu': 4, 'fri': 5, 'sat': 6};
@@ -104,26 +101,7 @@ class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 12, top: 12),
-            child: IconButton(
-              style: IconButton.styleFrom(
-                  backgroundColor: Colors.deepPurple.shade200,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12))),
-              onPressed: () {
-                pushScreenWithNavBar(context, ContactPage());
-              },
-              icon: RotatedBox(
-                quarterTurns: 1,
-                child: Icon(Icons.phone_enabled_outlined),
-              ),
-            ),
-          )
-        ],
-      ),
+      appBar: AppBar(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -206,6 +184,8 @@ class _AboutPageState extends State<AboutPage> {
               _BookArrangementSection(entries: _bookArrangement),
 
             _DDCReferenceTable(),
+
+            _ContactSection(),
 
             SizedBox(height: 20),
           ],
@@ -509,6 +489,72 @@ class _DDCReferenceTable extends StatelessWidget {
   }
 }
 
+class _ContactSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Icon(Icons.phone_enabled, color: Colors.deepPurple, size: 22),
+              SizedBox(width: 8),
+              Text(
+                "Reach Us",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 12),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple.shade50,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: Colors.deepPurple.shade200),
+            ),
+            child: Column(
+              children: [
+                _ContactRow(icon: Icons.mail, text: "library@nitc.ac.in"),
+                SizedBox(height: 14),
+                _ContactRow(icon: Icons.phone, text: "0495-2286063"),
+                SizedBox(height: 14),
+                _ContactRow(
+                  icon: Icons.location_on_sharp,
+                  text: "NIT Calicut, NIT Campus P.O\nKozhikode, Kerala, India 673601",
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ContactRow extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  const _ContactRow({required this.icon, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon, color: Colors.deepPurple, size: 20),
+        SizedBox(width: 10),
+        Expanded(child: Text(text, style: TextStyle(fontSize: 14, color: Colors.black87))),
+      ],
+    );
+  }
+}
+
 class AboutImage extends StatelessWidget {
   final int id;
 
@@ -525,152 +571,5 @@ class AboutImage extends StatelessWidget {
               fit: BoxFit.fill,
               child: Image.asset("assets/about_img_$id.png"),
             )));
-  }
-}
-
-class ContactPage extends StatelessWidget {
-  const ContactPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () => Navigator.of(context).pop(),
-            icon: Icon(Icons.arrow_back)),
-      ),
-      body: Padding(
-        padding:
-            const EdgeInsets.symmetric(horizontal: 68.0, vertical: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Reach Us",
-                style:
-                    TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
-            SizedBox(
-              height: 8,
-            ),
-            Wrap(
-              children: [
-                Text(
-                    "If you have any suggestions, questions, or need any assistance with anything related to library, dont hesistate to reach out-our team is always here and happy to reach you")
-              ],
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            IconWithText(icon: Icons.mail, text: "library@nitc.ac.in"),
-            SizedBox(
-              height: 8,
-            ),
-            IconWithText(icon: Icons.phone, text: "04952286063"),
-            SizedBox(
-              height: 8,
-            ),
-            IconWithText(
-                icon: Icons.location_on_sharp,
-                text:
-                    "NIT Calicut, NIT Campus P.O\nKozhikode,Kerala, India\n673601"),
-            SizedBox(
-              height: 16,
-            ),
-            SocialSection()
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class IconWithText extends StatelessWidget {
-  final IconData? icon;
-  final String text;
-
-  const IconWithText({
-    super.key,
-    required this.icon,
-    required this.text,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return FittedBox(
-      child: Row(
-        children: [
-          Icon(icon, color: Colors.deepPurple),
-          SizedBox(
-            width: 8,
-          ),
-          Text(text)
-        ],
-      ),
-    );
-  }
-}
-
-class SocialSection extends StatefulWidget {
-  const SocialSection({super.key});
-
-  @override
-  State<SocialSection> createState() => _SocialSectionState();
-}
-
-class _SocialSectionState extends State<SocialSection> {
-  Future<void> _launchUrl(String url) async {
-    final Uri uri = Uri.parse(url);
-    try {
-      if (!await launchUrl(uri, mode: LaunchMode.platformDefault)) {
-        _showErrorSnackBar("Could not launch $url");
-      }
-    } catch (e) {
-      _showErrorSnackBar("An error occured $e");
-    }
-  }
-
-  void _showErrorSnackBar(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-        backgroundColor: Colors.red,
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FittedBox(
-      child: Row(
-        children: [
-          Text("Follow Us",
-              style:
-                  TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
-          IconButton(
-            icon: FaIcon(FontAwesomeIcons.instagram,
-                color: Colors.deepPurple),
-            onPressed: () => _launchUrl(
-                "https://www.youtube.com/watch?v=dQw4w9WgXcQ"), //! PUT ACTUAL URLS
-          ),
-          IconButton(
-            icon:
-                FaIcon(FontAwesomeIcons.xTwitter, color: Colors.deepPurple),
-            onPressed: () => _launchUrl(
-                "https://www.youtube.com/watch?v=dQw4w9WgXcQ"), //! PUT ACTUAL URLS
-          ),
-          IconButton(
-            icon: FaIcon(FontAwesomeIcons.facebook,
-                color: Colors.deepPurple),
-            onPressed: () => _launchUrl(
-                "https://www.youtube.com/watch?v=dQw4w9WgXcQ"), //! PUT ACTUAL URLS
-          ),
-          IconButton(
-            icon: FaIcon(FontAwesomeIcons.linkedinIn,
-                color: Colors.deepPurple),
-            onPressed: () => _launchUrl(
-                "https://www.youtube.com/watch?v=dQw4w9WgXcQ"), //! PUT ACTUAL URLS
-          ),
-        ],
-      ),
-    );
   }
 }
